4526bca8788b2d3e45066acf1f9b43c40693edef
AEHTomaszRokoszynski <trokoszynski1pl@gmail.com>
2024-04-07 15:22:48 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
+
+import geometry.ColoredCircle;
+import geometry.Point;
+
 public class Main {
     public static void main(String[] args) {
         Point center = new Point(1.0, 2.0);
         double radius = 3.0;
+        String color = "blue";
         
-        Circle circle = new Circle(center, radius);
+        ColoredCircle coloredCircle = new ColoredCircle(center, radius, color);
         
-        System.out.println("Center: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
-        System.out.println("Radius: " + circle.getRadius());
-        System.out.println("Perimeter: " + circle.calculatePerimeter());
+        System.out.println("Center: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
+        System.out.println("Radius: " + coloredCircle.getRadius());
+        System.out.println("Color: " + coloredCircle.getColor());
+        System.out.println("Perimeter: " + coloredCircle.calculatePerimeter());
     }
 }
