881f3d14cc9f06dc5ae2f8c9d9575f81c599e27c
AEHTomaszRokoszynski <trokoszynski1pl@gmail.com>
2024-04-07 15:13:41 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-
-
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point(3.0, 4.0);
+        Point center = new Point(1.0, 2.0);
+        double radius = 3.0;
         
-        System.out.println("Coordinates: (" + point.getX() + ", " + point.getY() + ")");
+        Circle circle = new Circle(center, radius);
+        
+        System.out.println("Center: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Perimeter: " + circle.calculatePerimeter());
     }
 }
