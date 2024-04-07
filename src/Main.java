1d4b84d9f0bc20a39ff94c09e186b48675afa449
AEHTomaszRokoszynski <trokoszynski1pl@gmail.com>
2024-04-07 15:11:53 +0200

    zad1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(3.0, 4.0);
+        
+        System.out.println("Coordinates: (" + point.getX() + ", " + point.getY() + ")");
+    }
+}
